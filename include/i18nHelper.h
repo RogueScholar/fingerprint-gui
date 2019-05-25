eebfe5f7eb54ceee28e5e090a3c3f1c3f378e8b6
Peter J. Mello <admin@petermello.net>
2019-05-25 08:44:11 -0700

    Exercise option to relicense project under GPL-3+

--- include/i18nHelper.h
+++ include/i18nHelper.h
@@ -1,3 +1,28 @@
+ /*
+  * Project "Fingerprint GUI": Services for fingerprint authentication on Linux
+  * Module: i18nHelper.h
+  * Purpose: Internationalization definitions
+  * SPDX-License-Identifier: GPL-3.0-or-later
+  *
+  * @author  Wolfgang Ullrich
+  * Copyright (C) 2008-2016 Wolfgang Ullrich
+  */
+
+ /*
+  * This program is free software: you can redistribute it and/or modify it
+  * under the terms of the GNU General Public License as published by the Free
+  * Software Foundation, either version 3 of the License, or (at your option)
+  * any later version.
+  *
+  * This program is distributed in the hope that it will be useful, but WITHOUT
+  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+  * more details.
+  *
+  * You should have received a copy of the GNU General Public License along
+  * with this program. If not, see <https://www.gnu.org/licenses/>.
+  */
+
 #ifndef I18NHELPER_H
 #define I18NHELPER_H
 #include <QLocale>
