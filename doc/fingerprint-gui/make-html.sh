5ca515d0056b560159c5ef3900d7c2e9bc99e22e
Restyled.io <commits@restyled.io>
2021-06-29 08:06:56 +0000

    Restyled by shfmt

--- doc/fingerprint-gui/make-html.sh
+++ doc/fingerprint-gui/make-html.sh
@@ -2,11 +2,10 @@
 
 # simple script to make new html-files from xml
 
-XML=`ls *.xml`
+XML=$(ls *.xml)
 echo "$XML"
 rm *.html
 
-for WHAT in "$XML" ; do
-xmlto html-nochunks "$WHAT"
+for WHAT in "$XML"; do
+  xmlto html-nochunks "$WHAT"
 done
-
